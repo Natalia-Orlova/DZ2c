0aae6efd29da3b84a545a7e0261b50177ebf19bb
NataliaOrlova <natakarelskaya@yandex.ru>
2022-08-10 20:13:05 +0400

    добавила решение task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -4,3 +4,23 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void ThirdDigit(int number)
+{
+    if (number < 0) number = number * (-1);
+    if (number < 100)
+        Console.WriteLine($"В указанном числе нет 3-й цифры");
+    else
+    {
+        while (number > 999)
+        {
+            number = number / 10;
+        }
+        Console.WriteLine($"Третья цифра указанного числа: {number % 10} ");
+    }
+}
+
+ThirdDigit (num);
