4a6176025084f1dbf1982039058bb8d3703a44bb
NataliaOrlova <natakarelskaya@yandex.ru>
2022-08-10 18:12:22 +0400

    добавила решение task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -4,3 +4,20 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.WriteLine("Введите цифру от 1 до 7: ");
+int days = Convert.ToInt32(Console.ReadLine());
+
+void Holiday (int day)
+{
+    if (day > 0 && day < 8)
+{
+    if (day == 6 || day == 7) Console.WriteLine($"{day} -> выходной");
+    else
+        Console.WriteLine($"{day} -> не выходной");
+}
+else
+    Console.WriteLine($"цифра {day} не соответствует дню недели");
+}
+
+Holiday (days);
